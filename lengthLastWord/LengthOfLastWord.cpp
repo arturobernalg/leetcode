2770c849086ab77d32fc46691b7c55c4d2d44f6a
Arturo Bernal <arturobernalg@gmail.com>
2022-05-11 18:14:06 +0200

    include vector

--- lengthLastWord/LengthOfLastWord.cpp
+++ lengthLastWord/LengthOfLastWord.cpp
@@ -2,12 +2,13 @@
 // Created by Arturo Bernal on 9/5/22.
 //
 
+#include <vector>
 #include "LengthOfLastWord.h"
 
 int main() {
     LengthOfLastWord lengthOfLastWord;
 
-    string s = "abc";
+    string s = "Hello World";
     lengthOfLastWord.lengthOfLastWord(s);
 }
 
@@ -15,8 +16,32 @@ int main() {
 int LengthOfLastWord::lengthOfLastWord(string &s){
 
 
-    string delimiter = " ";
-    string token = s.substr(0, s.find(delimiter));
+
+    char char_array_haystack[s.length() + 1];
+    strcpy(char_array_haystack, s.c_str());
+
+    vector<string> myVector;
+
+    for (int i = 0; i < strlen(char_array_haystack); ++i) {
+        // first position blank
+        if (isblank(char_array_haystack[i]) && i ==0){
+            continue;
+        }else{
+            if (isblank(char_array_haystack[i])){
+                int j =i+1;
+                string concat;
+                while (!isblank(char_array_haystack[j])){
+                    j++;
+                    concat+= char_array_haystack[j];
+                }
+
+                myVector.push_back(concat);
+
+            }
+        }
+
+
+    }
 
     return 0;
 }
