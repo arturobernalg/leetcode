da5ca27b4b3be7cdd70de122d77ad0e4a21bf8fb
Arturo Bernal <arturobernalg@gmail.com>
2022-05-23 21:33:40 +0200

    Add Symmetric Tree class

---
+++ tree/symmetricTree/SymmetricTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Arturo Bernal on 15/5/22.
+//
+
+#include "SymmetricTree.h"
---
+++ tree/symmetricTree/SymmetricTree.h
@@ -0,0 +1,19 @@
+//
+// Created by Arturo Bernal on 15/5/22.
+//
+
+#ifndef LEETCODE_SYMMETRICTREE_H
+#define LEETCODE_SYMMETRICTREE_H
+
+
+#include "../sameTree/SameTree.h"
+
+class SymmetricTree {
+public:
+    bool isSymmetric(TreeNode* root) {
+
+    }
+};
+
+
+#endif //LEETCODE_SYMMETRICTREE_H
