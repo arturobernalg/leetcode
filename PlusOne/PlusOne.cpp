fdd90434f492c76aaecd432834b7e957f130a350
Arturo Bernal <arturo.bernal@unifiedpost.com>
2022-05-31 21:08:34 +0200

    add

--- PlusOne/PlusOne.cpp
+++ PlusOne/PlusOne.cpp
@@ -8,10 +8,11 @@
 
 int main() {
     PlusOne plusOne;
-    //std::vector<int> vector = {6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3};
+    std::vector<int> vector = {6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3};
     //std::vector<int> vector = {9,9};
     //std::vector<int> vector = {1,2,3};
-    std::vector<int> vector = {8,9,9,9};
+    //std::vector<int> vector = {8,9,9,9};
+    //std::vector<int> vector = {9};
     std::vector<int> out = plusOne.plusOne(vector);
 
     for (int i: out)
@@ -24,7 +25,15 @@ std::vector<int> PlusOne::plusOne(std::vector<int> &digits) {
 
     int rest = 0;
     for (int i = digits.size()-1; i >= 0 ; i--) {
-        if (digits[i] + rest + 1 > 9){
+        int needSum = 0;    
+        if (i == digits.size()-1)
+        {
+            needSum = 1;
+        }else{
+            needSum = 0;
+        }
+        
+        if (digits[i] + rest + needSum > 9){
             digits[i] = 0;
             rest = 1;
         }else {
